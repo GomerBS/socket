828775956136292436f3eb1da4223579d52316b5
GomerBS <18bs032@esisalama.org>
2022-08-13 22:39:46 +0200

    commit client conf

--- src/main/java/socket/Client.java
+++ src/main/java/socket/Client.java
@@ -1,12 +1,17 @@
 package socket;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 7777);
+
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            printWriter.println("Bonjour");
+            printWriter.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
