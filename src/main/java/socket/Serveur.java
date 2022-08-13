c883e96256555a249ef40287ff6a14d956dc5a80
GomerBS <18bs032@esisalama.org>
2022-08-13 22:45:27 +0200

    commit serveur conf

--- src/main/java/socket/Serveur.java
+++ src/main/java/socket/Serveur.java
@@ -1,6 +1,8 @@
 package socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,6 +12,10 @@ public class Serveur {
             ServerSocket serverSocket = new ServerSocket(7777);
             Socket s = serverSocket.accept();
             System.out.println("Client connected");
+            InputStreamReader inputStreamReader = new InputStreamReader(s.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String msg = bufferedReader.readLine();
+            System.out.println("Client: "+ msg);
         } catch (IOException e) {
             e.printStackTrace();
         }
